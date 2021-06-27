be71f8776f2c5ffbf92e069db8de785c7b085eb2
Sheshank <sheshank1225@gmail.com>
2021-06-27 21:26:27 +0530

    Added Lab and other assets and updated scripts

--- Assets/Scrpits/PlayerMovements.cs
+++ Assets/Scrpits/PlayerMovements.cs
@@ -6,35 +6,38 @@ public class PlayerMovements : MonoBehaviour
 {
 
     public Rigidbody rb;
+    public MeshCollider col;
 
+    public bool ground;
     // Start is called before the first frame update
-    void Start()
+    void GroundCollision()
     {
-        
+        ground = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
+        if (Input.GetKey("d"))
         {
             rb.AddForce(500f * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey("s"))
+        if (Input.GetKey("a"))
         {
             rb.AddForce(-500f * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey("a"))
+        if (Input.GetKey("w"))
         {
             rb.AddForce(0, 0, 500f * Time.deltaTime);
         }
-        if (Input.GetKey("d"))
+        if (Input.GetKey("s"))
         {
             rb.AddForce(0, 0, -500f * Time.deltaTime);
         }
-        if (Input.GetKey("q"))
+        if (Input.GetKey("q") && ground)
         {
-            rb.AddForce(0, 1000f * Time.deltaTime, 0);
+            rb.AddForce(0,800,0);
+            ground = false;
         }
     }
 
